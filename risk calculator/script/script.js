c3ca1921fc5855c0d964f29854b33c0bdbc8ad6d
Cryptum45 <bir19019@byui.edu>
2022-05-24 15:40:52 -0600

    Risk game

---
+++ risk calculator/script/script.js
@@ -0,0 +1,51 @@
+
+const buttons = document.querySelectorAll('button');
+
+//**************Math for reinforcements********************/
+
+const reinforcements = function(number){
+
+return 'With ' + number + ' territories' + ' You will get ' + (Math.floor(number / 3) || 1) + ' Reinforcements'
+
+};
+
+/*******Not a number************************/
+const wrongInput = function(number){
+  if (number === NaN){
+  return 'Must be a number'}
+    if (number > 0 ){
+      return reinforcements(number) 
+    }
+    else { return 'Must be postive'}
+};
+/***************************************/
+buttons.forEach(function(button) {
+
+  button.addEventListener('click', function() {
+    const input = this.parentNode.querySelector('input')
+    let numberOF = input.value;
+    input.value = '';
+    const list = this.parentNode.nextElementSibling.querySelector('ul')
+    numberOF = wrongInput(+numberOF)
+
+    const numberOf2 = document.createElement('li');
+    const textList = document.createElement('span');
+    const deleteButton = document.createElement('button');
+  
+    list.appendChild(numberOf2);
+    numberOf2.appendChild(textList)
+    textList.textContent = numberOF;
+    
+    
+    numberOf2.appendChild(deleteButton);
+    deleteButton.textContent = '❌';
+  
+    deleteButton.addEventListener('click', () => {
+      list.removeChild(numberOf2);
+    });
+  
+    input.focus();
+  });
+
+});
+
